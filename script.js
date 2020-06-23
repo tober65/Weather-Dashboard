1c094373b68a8477d64b44c5696d387af80fe406
tober65 <timothy.ober@gmail.com>
2020-06-23 10:47:01 -0700

    Implemented startup forecast

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var APIKey = "4c2e4d333c6901d5121a0e53238cf3ea";
-var now = moment();
 var cityList = [];
 
 var cityListString = localStorage.getItem("cityList");
@@ -10,26 +9,40 @@ if (cityListString !== null) {
 
 $("#history-buttons-div").empty();
 
-for(var i = 0; i < cityList.length; i++) {
+for (var i = 0; i < cityList.length; i++) {
     var newBtn = $("<button>");
     newBtn.text(cityList[i]);
     $("#history-buttons-div").prepend(newBtn);
 }
 
+if (cityList.length > 0) {
+    getWeather(cityList[cityList.length - 1]);
+}
+
+$("#history-buttons-div").on("click", "button", function (event) {
+    getWeather(event.target.textContent);
+});
+
 $("#search-button").on("click", function (event) {
     var city = $("#city-input").val();
 
-    cityList.push(city);
-    localStorage.setItem("cityList", JSON.stringify(cityList));
+    getWeather(city);
 
-    $("#history-buttons-div").empty();
+    if (cityList.indexOf(city) === -1) {
+        cityList.push(city);
+        localStorage.setItem("cityList", JSON.stringify(cityList));
 
-    for(var i = 0; i < cityList.length; i++) {
-        var newBtn = $("<button>");
-        newBtn.text(cityList[i]);
-        $("#history-buttons-div").prepend(newBtn);
+        $("#history-buttons-div").empty();
+
+        for (var i = 0; i < cityList.length; i++) {
+            var newBtn = $("<button>");
+            newBtn.text(cityList[i]);
+            $("#history-buttons-div").prepend(newBtn);
+        }
     }
+});
 
+function getWeather(city) {
     var queryURL =
         "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" +
         APIKey;
@@ -74,33 +87,33 @@ $("#search-button").on("click", function (event) {
             $("#uv-value").text(response.value);
         });
 
-        $("#city-name").text(response.name + " " + now.format("M/D/YYYY"));
+        $("#city-name").text(response.name + " " + moment().format("M/D/YYYY"));
         $("#today-wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
         $("#today-humidity").text("Humidity: " + response.main.humidity + "%");
         $("#today-temp").text("Temp: " + response.main.temp + " F");
 
-        document.getElementById("#day1").children[0].textContent = now.add(1, 'days').format("M/D/YYYY");
-        document.getElementById("#day2").children[0].textContent = now.add(1, 'days').format("M/D/YYYY");
-        document.getElementById("#day3").children[0].textContent = now.add(1, 'days').format("M/D/YYYY");
-        document.getElementById("#day4").children[0].textContent = now.add(1, 'days').format("M/D/YYYY");
-        document.getElementById("#day5").children[0].textContent = now.add(1, 'days').format("M/D/YYYY");
+        document.getElementById("#day1").children[0].textContent = moment().add(1, 'days').format("M/D/YYYY");
+        document.getElementById("#day2").children[0].textContent = moment().add(2, 'days').format("M/D/YYYY");
+        document.getElementById("#day3").children[0].textContent = moment().add(3, 'days').format("M/D/YYYY");
+        document.getElementById("#day4").children[0].textContent = moment().add(4, 'days').format("M/D/YYYY");
+        document.getElementById("#day5").children[0].textContent = moment().add(5, 'days').format("M/D/YYYY");
     });
 
     $.ajax({
         url: fiveDayQueryURL,
         method: "GET",
     }).then(function (response) {
-        document.getElementById("#day1").children[2].textContent = "Temp: " + response.list[1].main.temp + " F";
-        document.getElementById("#day2").children[2].textContent = "Temp: " + response.list[9].main.temp + " F";
-        document.getElementById("#day3").children[2].textContent = "Temp: " + response.list[17].main.temp + " F";
-        document.getElementById("#day4").children[2].textContent = "Temp: " + response.list[25].main.temp + " F";
-        document.getElementById("#day5").children[2].textContent = "Temp: " + response.list[33].main.temp + " F";
+        document.getElementById("#day1").children[2].textContent = "Temp: " + response.list[7].main.temp + " F";
+        document.getElementById("#day2").children[2].textContent = "Temp: " + response.list[15].main.temp + " F";
+        document.getElementById("#day3").children[2].textContent = "Temp: " + response.list[23].main.temp + " F";
+        document.getElementById("#day4").children[2].textContent = "Temp: " + response.list[31].main.temp + " F";
+        document.getElementById("#day5").children[2].textContent = "Temp: " + response.list[39].main.temp + " F";
 
-        document.getElementById("#day1").children[3].textContent = "Humidity: " + response.list[1].main.humidity + "%";
-        document.getElementById("#day2").children[3].textContent = "Humidity: " + response.list[9].main.humidity + "%";
-        document.getElementById("#day3").children[3].textContent = "Humidity: " + response.list[17].main.humidity + "%";
-        document.getElementById("#day4").children[3].textContent = "Humidity: " + response.list[25].main.humidity + "%";
-        document.getElementById("#day5").children[3].textContent = "Humidity: " + response.list[33].main.humidity + "%";
+        document.getElementById("#day1").children[3].textContent = "Humidity: " + response.list[7].main.humidity + "%";
+        document.getElementById("#day2").children[3].textContent = "Humidity: " + response.list[15].main.humidity + "%";
+        document.getElementById("#day3").children[3].textContent = "Humidity: " + response.list[23].main.humidity + "%";
+        document.getElementById("#day4").children[3].textContent = "Humidity: " + response.list[31].main.humidity + "%";
+        document.getElementById("#day5").children[3].textContent = "Humidity: " + response.list[39].main.humidity + "%";
 
         var iconId = response.list[1].weather[0].icon;
         document.getElementById("#day1").children[1].innerHTML = "<img src='http://openweathermap.org/img/wn/" + iconId + ".png'>";
@@ -117,5 +130,4 @@ $("#search-button").on("click", function (event) {
         iconId = response.list[33].weather[0].icon;
         document.getElementById("#day5").children[1].innerHTML = "<img src='http://openweathermap.org/img/wn/" + iconId + ".png'>";
     });
-});
-
+}
